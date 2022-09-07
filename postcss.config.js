8f55e26542aa1bbc8e6127363d1eecc8a6119673
Yordani Bonilla <b.yordani93@gmail.com>
2022-09-07 01:58:39 -0500

    tailwind working babel webpack configured

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     plugins: [
         require("tailwindcss")("./tailwind.config.js"),
-        require("autoprefixer")
+        require("autoprefixer"),
+        require("postcss-preset-env")
+
     ],
   }
   
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  purge: ["./front/dist/*.html","./front/src/*.{js,jsx}"],
   content: [],
   theme: {
     extend: {},
